fccca66959d8f9019e42c6561da53e0baa3cde00
Janis <janis_k@hotmail.com>
2026-01-05 10:48:52 +0100

    Set up tailwind/daisyui/icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello Nuxt world! (Root app.vue content)
+    </h1>
+
+    <NuxtPage />
   </div>
 </template>
