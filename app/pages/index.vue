d4c2ba6a7edf6002f2443fa09a18d61684ce3c8d
Janis Karlsson <janis_k@hotmail.com>
2026-01-05 10:50:17 +0100

    Set up tailwind/daisyui/icons (#6)

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2 class="text-3xl">
+      This is the content from the Home Page (pages/index.vue)
+    </h2>
+    <p>This content is rendered inside the &lt;NuxtPage /&gt; component in app.vue.</p>
+
+    <button class="btn btn-secondary mt-4">
+      View map <Icon name="tabler:map" />
+    </button>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  devtools: { enabled: false },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
